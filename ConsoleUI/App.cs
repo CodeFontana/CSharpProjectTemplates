d309cd3c8c65cab2f23553b5571a5f4210047bf2
Brian Fontana <brian.fontana@fiserv.com>
2022-02-03 23:47:05 -0500

    Add console project template

---
+++ ConsoleUI/App.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleUI;
+
+public class App : IHostedService
+{
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly IConfiguration _config;
+    private readonly ILogger<App> _logger;
+
+    public App(IHostApplicationLifetime hostApplicationLifetime,
+               IConfiguration configuration,
+               ILogger<App> logger)
+    {
+        _hostApplicationLifetime = hostApplicationLifetime;
+        _config = configuration;
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _hostApplicationLifetime.ApplicationStarted.Register(async () =>
+        {
+            try
+            {
+                await Task.Yield(); // https://github.com/dotnet/runtime/issues/36063
+                await Task.Delay(1000); // Additional delay for Microsoft.Hosting.Lifetime messages
+                await Run();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception!");
+            }
+            finally
+            {
+                _hostApplicationLifetime.StopApplication();
+            }
+        });
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task Run()
+    {
+        _logger.LogInformation("Hello, World!");
+        await Task.Delay(1);
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleUI;
+
+class Program
+{
+    public static IConfigurationRoot? Configuration { get; set; }
+
+    static async Task Main(string[] args)
+    {
+        string? env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
+
+        await Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(config =>
+            {
+                config.SetBasePath(Directory.GetCurrentDirectory());
+                config.AddJsonFile("appsettings.json", true, true);
+                config.AddJsonFile($"appsettings.{env}.json", true, true);
+                config.AddEnvironmentVariables();
+
+                if (isDevelopment)
+                {
+                    config.AddUserSecrets<Program>(optional: true);
+                }
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<App>();
+            })
+            .ConfigureLogging((hostContext, config) =>
+            {
+                config.ClearProviders();
+                config.AddSimpleConsole(options =>
+                {
+                    options.SingleLine = true;
+                    options.TimestampFormat = "MM/dd/yyyy HH:mm:ss ";
+                });
+            })
+            .RunConsoleAsync();
+    }
+}
