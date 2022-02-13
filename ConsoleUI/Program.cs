a3b2e8e91e25253f2773241a01aadf000c99677b
Brian Fontana <brian@codefoxtrot.com>
2022-02-13 01:22:00 -0500

    Add Serilog logging by default

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ConsoleUI;
 
@@ -11,35 +11,50 @@ class Program
 
     static async Task Main(string[] args)
     {
-        string? env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-        bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
+        string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u4} [{SourceContext}] {Message:lj}{NewLine}{Exception}";
 
-        await Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration(config =>
-            {
-                config.SetBasePath(Directory.GetCurrentDirectory());
-                config.AddJsonFile("appsettings.json", true, true);
-                config.AddJsonFile($"appsettings.{env}.json", true, true);
-                config.AddEnvironmentVariables();
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Console(
+                outputTemplate: outputTemplate)
+            .WriteTo.File(
+                path: "ConsoleUI-.log",
+                outputTemplate: outputTemplate,
+                rollOnFileSizeLimit: true,
+                fileSizeLimitBytes: 1073741824,
+                retainedFileCountLimit: 31,
+                rollingInterval: RollingInterval.Day)
+            .CreateBootstrapLogger();
 
-                if (isDevelopment)
+        try
+        {
+            string? env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+            bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
+
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config =>
                 {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appsettings.json", true, true);
+                    config.AddJsonFile($"appsettings.{env}.json", true, true);
                     config.AddUserSecrets<Program>(optional: true);
-                }
-            })
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddHostedService<App>();
-            })
-            .ConfigureLogging((hostContext, config) =>
-            {
-                config.ClearProviders();
-                config.AddSimpleConsole(options =>
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureServices((hostContext, services) =>
                 {
-                    options.SingleLine = true;
-                    options.TimestampFormat = "MM/dd/yyyy HH:mm:ss ";
-                });
-            })
-            .RunConsoleAsync();
+                    services.AddHostedService<App>();
+                })
+                .UseSerilog((context, services, loggerConfiguration) => 
+                    loggerConfiguration.ReadFrom.Configuration(context.Configuration))
+                .RunConsoleAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Unexpected error");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
