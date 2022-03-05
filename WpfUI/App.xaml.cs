6ea64ebf2695717bedaac41409b3b530dcf1d7ce
Brian Fontana <brian@codefoxtrot.com>
2022-03-05 11:48:27 -0500

    Employ constructor in App.xaml.cs to handle generic host setup

--- WpfUI/App.xaml.cs
+++ WpfUI/App.xaml.cs
@@ -8,64 +8,73 @@ using System.Windows;
 
 namespace WpfUI
 {
-	public partial class App : Application
-	{
-		private IHost _appHost;
+    public partial class App : Application
+    {
+        private IHost _appHost;
 
-		protected override async void OnStartup(StartupEventArgs e)
-		{
-			base.OnStartup(e);
-
-			try
-			{
-				string env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-				bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
-
-				_appHost = Host.CreateDefaultBuilder(e.Args)
-					.ConfigureAppConfiguration(config =>
-					{
-						config.SetBasePath(Directory.GetCurrentDirectory());
-						config.AddJsonFile("appsettings.json", true, true);
-						config.AddJsonFile($"appsettings.{env}.json", true, true);
-						config.AddUserSecrets<App>(optional: true);
-						config.AddEnvironmentVariables();
-					})
-					.ConfigureServices((hostContext, services) =>
-					{
-						//services.AddSingleton<MainWindowViewModel>();
-						services.AddSingleton<MainWindow>();
-					})
-					.UseSerilog((context, services, loggerConfiguration) =>
-						loggerConfiguration.ReadFrom.Configuration(context.Configuration))
-					.Build();
-
-				await _appHost.StartAsync();
-
-				var appWindow = _appHost.Services.GetService<MainWindow>();
-				//appWindow.DataContext = _appHost.Services.GetService<MainWindowViewModel>();
-				appWindow.Show();
-			}
-			catch (Exception ex)
-			{
-				Log.Fatal(ex, "Unexpected error");
-			}
-		}
-
-		protected override async void OnExit(ExitEventArgs e)
-		{
+        public App()
+        {
             try
             {
-				await _appHost.StopAsync();
-				Log.CloseAndFlush();
-			}
+                string env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+                bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
+
+                _appHost = Host.CreateDefaultBuilder()
+                    .ConfigureAppConfiguration(config =>
+                    {
+                        config.SetBasePath(Directory.GetCurrentDirectory());
+                        config.AddJsonFile("appsettings.json", true, true);
+                        config.AddJsonFile($"appsettings.{env}.json", true, true);
+                        config.AddUserSecrets<App>(optional: true);
+                        config.AddEnvironmentVariables();
+                    })
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddTransient<MainWindow>(s => new MainWindow()
+                        {
+                            
+                        });
+                    })
+                    .UseSerilog((context, services, loggerConfiguration) =>
+                        loggerConfiguration.ReadFrom.Configuration(context.Configuration))
+                    .Build();
+            }
             catch (Exception ex)
             {
-				Log.Fatal(ex, "Unexpected error");
-			}
-			finally
+                string type = ex.GetType().Name;
+                if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+                {
+                    throw;
+                }
+
+                Log.Fatal(ex, "Unexpected error");
+                Application.Current.Shutdown();
+            }
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _appHost.StartAsync();
+            MainWindow mainWindow = _appHost.Services.GetService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            try
             {
-				base.OnExit(e);
-			}
-		}
-	}
+                if (_appHost != null) await _appHost.StopAsync();
+                Log.CloseAndFlush();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Unexpected error");
+            }
+            finally
+            {
+                base.OnExit(e);
+            }
+        }
+    }
 }
