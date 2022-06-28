362a10e612431159710b6187f70c4da3de35e0ad
Brian Fontana <brian@codefoxtrot.com>
2022-06-28 17:02:22 -0400

    Update Blazor Client template
    Mostly color theme updates, but also store the user's theme preference in browser local storage

--- BlazorWasmUI/Program.cs
+++ BlazorWasmUI/Program.cs
@@ -1,12 +1,15 @@
+using Blazored.LocalStorage;
 using BlazorWasmUI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
