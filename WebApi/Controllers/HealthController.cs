338b1bb57a54a1049b263c2fede3eaf54206b362
Brian Fontana <brian@codefoxtrot.com>
2022-07-12 22:18:58 -0400

    Add simple health controller

---
+++ WebApi/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersionNeutral]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    [Route("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("Still alive");
+    }
+}
