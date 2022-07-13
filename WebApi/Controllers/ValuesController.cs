a9c8824d5f162221a84ad42e3c8a70ffbd1f6f8a
Brian Fontana <brian@codefoxtrot.com>
2022-07-13 16:25:00 -0400

    Add ValuesController to template
    Makes sense to have a quick place to test, which can easily be deleted when removing the template

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersionNeutral]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+
+    }
+}
