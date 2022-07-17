4aa9257f530dab14988caae19b7db55063db91b6
Brian Fontana <brian@codefoxtrot.com>
2022-07-17 14:32:22 -0400

    Condense Program.cs

--- BlazorServerUI/Program.cs
+++ BlazorServerUI/Program.cs
@@ -1,32 +1,24 @@
 using Blazored.LocalStorage;
 using MudBlazor.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseStaticWebAssets();
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor(); 
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
+WebApplication app = builder.Build();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() == false)
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
