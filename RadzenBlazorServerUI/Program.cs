aa98ceb3bc54f7fcb4d80f591726e58f8ef698dc
Brian Fontana <brian@codefoxtrot.com>
2022-09-27 11:04:59 -0400

    Add Radzen Blazor Server template

---
+++ RadzenBlazorServerUI/Program.cs
@@ -0,0 +1,22 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment() == false)
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.Run();
