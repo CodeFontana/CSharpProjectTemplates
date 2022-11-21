e02862410eaedf812aa9d96c4406822556f87abc
Brian Fontana <brian@codefoxtrot.com>
2022-11-20 20:31:55 -0500

    Update RadzenBlazorServerUI project to .NET7 and switch to Serilog logging

--- RadzenBlazorServerUI/Program.cs
+++ RadzenBlazorServerUI/Program.cs
@@ -1,22 +1,47 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<DialogService>();
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<TooltipService>();
-builder.Services.AddScoped<ContextMenuService>();
+using Radzen;
+using Serilog;
+using Serilog.Events;
 
-WebApplication app = builder.Build();
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Default", LogEventLevel.Debug)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
 
-if (app.Environment.IsDevelopment() == false)
+try
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+    {
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    builder.Services.AddScoped<DialogService>();
+    builder.Services.AddScoped<NotificationService>();
+    builder.Services.AddScoped<TooltipService>();
+    builder.Services.AddScoped<ContextMenuService>();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-app.Run();
+    WebApplication app = builder.Build();
+
+    if (app.Environment.IsDevelopment() == false)
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
