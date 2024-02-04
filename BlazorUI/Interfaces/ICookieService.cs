ce64d32130c197e4b2ce5a65548b1bde5a1c83de
Brian Fontana <brian@codefoxtrot.com>
2024-02-04 16:31:16 -0500

    Pull up SetCookie overload to interface

--- BlazorUI/Interfaces/ICookieService.cs
+++ BlazorUI/Interfaces/ICookieService.cs
@@ -1,7 +1,9 @@
-﻿namespace BlazorUI.Interfaces;
+﻿
+namespace BlazorUI.Interfaces;
 
 internal interface ICookieService
 {
     T? GetCookie<T>(string key);
     void SetCookie<T>(string key, T value, int daysToExpire = 30);
+    void SetCookie<T>(string key, T value, DateTimeOffset? expiry);
 }
