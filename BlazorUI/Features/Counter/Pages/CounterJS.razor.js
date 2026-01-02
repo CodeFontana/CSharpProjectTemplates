a6bc0100e59de1edef12b883f5efbc3ac7581d1e
Brian Fontana <brian@codefoxtrot.com>
2026-01-02 12:00:14 -0500

    Add JS counter page sample based on BlazorJSComponents

---
+++ BlazorUI/Features/Counter/Pages/CounterJS.razor.js
@@ -0,0 +1,24 @@
+﻿export default class extends BlazorJSComponents.Component {
+    attach() {
+        this.currentCount = 0;
+    }
+
+    setParameters({ curCount, btnIncrement, btnReset }) {
+        this.curCount = curCount;
+        this.setEventListener(btnIncrement, 'click', () => {
+            this.currentCount += 1;
+            this.render();
+        });
+        this.setEventListener(btnReset, 'click', () => {
+            this.currentCount = 0;
+            this.render();
+        });
+        this.render();
+    }
+
+    // A helper to apply changes to DOM content.
+    // Not called automatically.
+    render() {
+        this.curCount.innerText = `Current count: ${this.currentCount}`;
+    }
+}
